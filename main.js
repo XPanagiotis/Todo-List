9f524327d604cc9a9a362cb3ec4ea49e484287e0
Panagiotis Xenos <xenospanagiot@gmail.com>
2024-04-17 14:59:33 +0300

    Create and remove new categories

--- main.js
+++ main.js
@@ -47,6 +47,17 @@ const styles = (function(){
   }
 })()
 
+const mainScreen = document.getElementById('main-screen');
+const categoriesScreen = document.getElementById('category-screen');
+const sortScreen = document.getElementById('sort-screen');
+
+const categoriesSidebarBtn = document.getElementById('categories-button');
+const upcomingSidebarBtn = document.getElementById('upcoming-button');
+const sortSidebarBtn = document.getElementById('sort-sidebar-button');
+
+upcomingSidebarBtn.addEventListener('click', () => events.emit('showUpcomingScreen'));
+categoriesSidebarBtn.addEventListener('click', () => events.emit('showCategoriesScreen'));
+sortSidebarBtn.addEventListener('click', () => events.emit('showSortScreen'));
 
 //todo controller module
   //handle DOM
@@ -62,7 +73,8 @@ const todoController = (function() {
   const submitBtn =  document.getElementById('save-task-button');
   const cancelBtn = document.getElementById('cancel-button');
 
-  const addNewTaskBtn = document.getElementById('add-task-button')
+  const addNewTaskBtn = document.getElementById('add-task-button');
+
 
   //form
   const title = document.getElementById('task-name');
@@ -74,12 +86,24 @@ const todoController = (function() {
   openSidebarBtn.addEventListener('click', styles.sidebarAnimation);
   addNewTaskBtn.addEventListener('click', displayForm);
   document.getElementById('side-bar-add-new-task').addEventListener('click', displayForm);
+
+  events.on('showUpcomingScreen', showUpcomingScreen);
   events.on('renderTodos', renderTodos);
   events.on('displayTodo', displayToDos);
+
+  function showUpcomingScreen() {
+    categoriesScreen.style.display = 'none';
+    sortScreen.style.display = 'none';
+
+    mainScreen.style.display = 'flex';
+  }
 
 
 
   function displayForm() {
+    //if we are in another screen and add new task is clicked we show the upcoming screen
+    if(mainScreen.style.display == 'none') events.emit('showUpcomingScreen');
+
     //if edit form is displayed we remove it and reset it
     if (newTodoInput.style.display == 'flex') {
       newTodoInput.style.display = 'none';
@@ -145,22 +169,30 @@ const todoController = (function() {
   }
 
   function addEditEL(todo, element) {
-    element.querySelector('.edit-button').addEventListener('click', () => {
+    element.querySelector('.edit-button').addEventListener('click', _edit)
+
+
+    function _edit() {
+
       events.on('showTask', () => {
         element.style.display = 'flex';
       })
 
-      //display edit form
-      const form = element.insertAdjacentElement('afterend', newTodoInput);
+
 
       //if another edit button is clicked we display all the todos
       let cards = document.querySelectorAll('.to-do-card');
       cards.forEach(card => {
         if(card.style.display == 'none') {
           card.style.display = 'flex';
+
         }
       });
 
+      //display edit form
+      const form = element.insertAdjacentElement('afterend', newTodoInput);
+
+      console.log(form)
       //hide the todo we want to edit
       element.style.display = 'none';
 
@@ -176,19 +208,19 @@ const todoController = (function() {
       submitBtn.style.cursor = 'pointer';
 
       //Bind events
-      cancelBtn.addEventListener('click', cancelEdit);
+      cancelBtn.addEventListener('click', cancelEdit, {once:true});
+      submitBtn.addEventListener('click', submitEdit, {once:true});
 
       title.addEventListener('keyup', () => {
         if (title.value == null || title.value == '') {
           submitBtn.style.opacity = '0.5';
           submitBtn.style.cursor = 'not-allowed';
-          //submitBtn.removeEventListener('click', editTodo);
+          submitBtn.removeEventListener('click', editTodo);
         }
         else {
           submitBtn.style.opacity = "1";
           submitBtn.style.cursor = 'pointer';
           //if user has input data we create a new to-do
-          submitBtn.addEventListener('click', submitEdit);
         }
       });
 
@@ -224,7 +256,8 @@ const todoController = (function() {
 
       //submitBtn.addEventListener('click', editTodo);
       newTodoInput.style.display = 'flex';
-    })
+    }
+    
   }
 
   function renderTodos(todos) {
@@ -320,7 +353,134 @@ const todoController = (function() {
 
      return iconEl
   }
+
+
+})();
+
+const categoryController = (function() {
+
+  //cashe DOM
+  const addCategoryBtn = document.getElementById('add-category-button');
+  const categoriesContainer = document.getElementById('categories-container')
+
+  //bind events
+  events.on('showCategoriesScreen', showCategoriesScreen);
+  addCategoryBtn.addEventListener('click', displayAddCategoryForm, {once:true});
+  events.on('renderCategories', renderCategories)
+
+  function showCategoriesScreen() {
+    mainScreen.style.display = 'none';
+    sortScreen.style.display = 'none';
+
+    categoriesScreen.style.display = 'flex';
+  }
+
+  function displayAddCategoryForm() {
+    const addCategoryForm = document.createElement('div');
+    addCategoryForm.setAttribute('class', 'new-category-input-container');
+    addCategoryForm.style.display = 'flex';
+
+    const categoryNameInput = document.createElement('input');
+    categoryNameInput.id = 'category-name-input';
+    categoryNameInput.setAttribute('placeholder', 'Category Name');
+
+    addCategoryForm.appendChild(categoryNameInput);
+
+    const dtnContainer = document.createElement('div');
+    dtnContainer.setAttribute('class', 'save-cancel-button-container');
+    addCategoryForm.appendChild(dtnContainer)
+    
+    const cancelBtn = document.createElement('button');
+    cancelBtn.setAttribute('class', 'cancel-button hover-effect');
+    cancelBtn.textContent = 'Cancel'
+    cancelBtn.id = 'cancel-category-button';
+
+    dtnContainer.appendChild(cancelBtn);
+
+    const saveBtn = document.createElement('button');
+    saveBtn.setAttribute('class', 'save-task-button');
+    saveBtn.textContent = 'Save'
+    saveBtn.id = 'save-category-button';
+
+    dtnContainer.appendChild(saveBtn);
+
+    //bind events
+    cancelBtn.addEventListener('click', () => {
+      addCategoryForm.remove();
+      addCategoryBtn.addEventListener('click', displayAddCategoryForm, {once:true});
+    });
+
+    categoryNameInput.addEventListener('keyup', () => {
+      if (categoryNameInput.value == null || categoryNameInput.value == '') {
+        saveBtn.style.opacity = '0.5';
+        saveBtn.style.cursor = 'not-allowed';
+      }
+      else {
+        saveBtn.style.opacity = "1";
+        saveBtn.style.cursor = 'pointer';
+        //if user has input data we create a new to-do
+        saveBtn.addEventListener('click', addCategory);
+      }
+    });
+
+    function addCategory() {
+      if(categoryNameInput.value != '') {
+        events.emit('createCategory', categoryNameInput.value);   
+        
+        //reset form
+        categoryNameInput.value = '';
+      }
+    }
+    
+    addCategoryBtn.insertAdjacentElement('beforebegin', addCategoryForm);
+  }
+
+  function renderCategories(categories) {
+    categoriesContainer.textContent = '';
+    categories.forEach(category => displayCategory(category));
+  }
+
+  function displayCategory(category) {
+    const categoryCard = document.createElement('div')
+    categoryCard.setAttribute('class', 'category-card');
+    //categoryCard.id = category.id;
+
+    const categoryName = document.createElement('div');
+    categoryName.textContent = category.name;
+    categoryName.setAttribute('class', 'category-name');
+    categoryCard.appendChild(categoryName);
+
+    const removeCategoryBtn = document.createElement('img');
+    removeCategoryBtn.src = './icons/remove-icon.svg';
+    removeCategoryBtn.setAttribute('class', 'hover-effect');
+    removeCategoryBtn.id = category.id;
+    categoryCard.appendChild(removeCategoryBtn);
+
+    //atach eventListener
+    removeCategoryBtn.addEventListener('click', (event) => {
+      const deleteBtn = event.target
+      const idToDelete = deleteBtn.id;
+
+      events.emit('deleteCategory', idToDelete);
+    })
+
+    categoriesContainer.insertAdjacentElement('afterbegin', categoryCard);
+  }
+
+})();
+
+const sortController = (function() {
+
+  events.on('showSortScreen', showSortScreen)
+
+  function showSortScreen() {
+    mainScreen.style.display = 'none';
+    categoriesScreen.style.display = 'none';
+
+    sortScreen.style.display = 'flex';
+  }
 })()
+
 
 
 //ToDos module
@@ -333,7 +493,7 @@ const toDosHandler = (function() {
   //initialize todos
   let todos = [];
 
-  const savedTodos = JSON.parse(localStorage.getItem('todos'))
+  const savedTodos = JSON.parse(localStorage.getItem('todos'));
   if(Array.isArray(savedTodos)) {
     todos = savedTodos;
     todos.forEach(todo =>  events.emit('displayTodo', todo))
@@ -342,7 +502,7 @@ const toDosHandler = (function() {
   //bind events
   events.on('create ToDo', createToDo);
   events.on('submitEdit', editTodo)
-
+  
   //create a new todo
   function createToDo(data) {
     //set unique id
@@ -384,4 +544,52 @@ const toDosHandler = (function() {
   function saveTodo() {
     localStorage.setItem('todos', JSON.stringify(todos));
   }
-})()
+})();
+
+
+const categoriesHandler = (function() {
+  let categories;
+
+  const savedCategories = JSON.parse(localStorage.getItem('categories'));
+  if(Array.isArray(savedCategories)) {
+    categories = savedCategories;
+  } else {
+    categories = [{
+      id: 1,
+      name: 'Groceries'
+    }, {
+      id: 2,
+      name: 'Super Market'
+    }]
+  }
+
+  //bind events
+  events.on('createCategory', createCategory);
+  events.on('deleteCategory', deleteCategory);
+  events.emit('renderCategories', categories);
+
+  function createCategory(category) {
+    const id = new Date().getTime();
+    categories.push({
+      id: id,
+      name: category
+    });
+    saveCategory();
+    events.emit('renderCategories', categories);
+  }
+
+  function deleteCategory(idToDelete) {
+    categories = categories.filter((category) => {
+      if(category.id == idToDelete) {
+        return false;
+      } else return true;
+    });
+
+    saveCategory();
+    events.emit('renderCategories', categories);
+  }
+
+  function saveCategory() {
+    localStorage.setItem('categories', JSON.stringify(categories));
+  }
+})();
